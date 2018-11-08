450dac82c04c5d4038f5fbda2c5d76edf977eaf7
DaviChalita <43867287+DaviChalita@users.noreply.github.com>
2018-11-08 19:03:06 -0200

    exemplo troca de mensagens

---
+++ EchoClient.py
@@ -0,0 +1,33 @@
+import socket
+import sys
+import pickle
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+
+server_address = ('localhost', 10000)
+print (sys.stderr, 'connecting to %s port %s' % server_address)
+sock.connect(server_address)
+
+try:
+
+    # Send data
+    r = input()
+    message = pickle.dumps(r)
+    print (sys.stderr, 'sending "%s"' % message)
+    sock.send(message)
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(message)
+
+    while amount_received < amount_expected:
+        data = sock.recv(16)
+        amount_received += len(data)
+        print (sys.stderr, 'received "%s"' % data)
+
+finally:
+    print (sys.stderr, 'closing socket')
+    sock.close()
